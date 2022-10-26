bb20432a01519a2ec9c0a8e353d21191d5a84edd
RobertoHiroshi <mrhiroshiroberto@yahoo.com.br>
2022-10-26 10:21:20 -0300

    adicionado boa parte dos inputs

--- main.c
+++ main.c
@@ -1,34 +1,83 @@
 #include <stdio.h>
 #include <locale.h>
 
-char nome[100];
-int idade;
-char endereco[100];
+typedef struct
+{
+   char rua[100];
+   int numero;
+   char bairro[100];
+   char cidade[100];
+   char estado[100];
+   int CEP;
+} Endereco;
+
+typedef struct
+{
+    char nome[100];
+    int idade;
+    int cpf;
+    int telefone;
+    Endereco endereco;
+
+} Paciente;
+
+Paciente paciente;
+
+void cadastrarPaciente(){
+    setlocale (LC_ALL, "");
+
+    printf("Digite o nome do/a paciente: ");
+    gets(paciente.nome);
+
+    printf("Digite o CPF do/a paciente: ");
+    scanf("%i[^\n]",&paciente.cpf);
+    getchar();
+
+    printf("Digite a idade do/a paciente: ");
+    scanf("%i[^\n]",&paciente.idade);
+    getchar();
+
+    printf("Digite o endereÃ§o do/a paciente \n");
+
+    printf("Digite a rua do/a paciente: ");
+    gets(paciente.endereco.rua);
+
+    printf("Digite o nÃºmero da rua do/a paciente: ");
+    scanf("%i[^\n]",&paciente.endereco.numero);
+
+    printf("Digite o bairro do/a paciente: ");
+    gets(paciente.endereco.bairro);
+
+    printf("Digite a cidade do/a paciente: ");
+    gets(paciente.endereco.cidade);
+
+    printf("Digite o estado do/a paciente: ");
+    gets(paciente.endereco.estado);
+
+    printf("Digite o CEP do/a paciente: ");
+    scanf("%i[^\n]",&paciente.endereco.CEP);
+    getchar();
+    
+    feedback();
+}
+
+void feedback(){
+    printf("\n\nPaciente cadastrado\nNome: %s\nCPF: %s\nTelefone: %i\nRua: %s\nNÃºmero: %i\nBairro: %s\nCidade: %s\nEstado: %s\nCEP: %i", 
+    paciente.nome, 
+    paciente.cpf, 
+    paciente.telefone, 
+    paciente.endereco.rua, 
+    paciente.endereco.numero, 
+    paciente.endereco.bairro, 
+    paciente.endereco.cidade, 
+    paciente.endereco.estado, 
+    paciente.endereco.CEP);
+
+    getchar();
+}
 
 int main()
 {
     cadastrarPaciente();
     return 0;
-}
-
-void cadastrarPaciente(){
-    //setlocale (LC_ALL, "");
-    printf("Digite o nome do/a paciente: ");
-    gets(nome);
-    //getchar();
-
-    printf("Digite a idade do/a paciente: ");
-    scanf("%d[^\n]",&idade);
-    getchar();
-
-    printf("Digite o endereço do/a paciente: ");
-    gets(endereco);
-    //getchar();
-
-    //scanf("%s %d %s", nome, idade, endereco);
-
-    printf("\n\nPaciente: %s\nIdade: %d\nEndereço: %s", nome, idade, endereco);
-
-
-    getchar();
 }
